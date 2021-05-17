5ad962b0cecd9e0b507b55b4bbc4ae08ab835ef7
lenguyenkhoi21 <lenguyenkhoi21@gmail.com>
2021-05-17 10:41:18 +0700

    Configure for Test, Development and Production enviorment with Open API 3. Integrate JPA Repository

--- src/main/java/com/nguyenkhoi/mongoddd/controller/HelloController.java
+++ src/main/java/com/nguyenkhoi/mongoddd/controller/HelloController.java
@@ -2,6 +2,10 @@ package com.nguyenkhoi.mongoddd.controller;
 
 import com.nguyenkhoi.mongoddd.document.Hello;
 import com.nguyenkhoi.mongoddd.service.HelloSerivce;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +18,16 @@ public class HelloController {
         this.serivce = serivce;
     }
 
+
     @GetMapping("/hello/{id}")
+    @Operation(summary = "Query hello table in database", description = "Info the id")
+    @Parameter(description = "id of hello")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Successfully"),
+                    @ApiResponse(responseCode = "500", description = "Bug")
+            }
+    )
     public Hello getHello(@PathVariable("id") int id) {
         return serivce.getHello(id);
     }
